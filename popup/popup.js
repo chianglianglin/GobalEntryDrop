dde782ee5b6db21ed6b94c06329b09de6446f7d6
jackson4 <jackson@gmail.com>
2024-12-19 18:15:16 +0800

    js  catch some data

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,19 @@
 // ElEMENTS
+const locationIdElement = document.getElementById("locationId")
+const startDateIdElement = document.getElementById("startDateId")
+const endDateIdElement = document.getElementById("endDateId")
+
+
+//button elements
+const startButton = document.getElementById("startButtonId")
+const endButton = document.getElementById("stopButtonId")
+
+
+startButton.onclick = function() {
+    console.log("startDate",startDateIdElement.value);
+}
+
+endButton.onclick = function() {
+    console.log("endDate", endDateIdElement.value);
+}
+
